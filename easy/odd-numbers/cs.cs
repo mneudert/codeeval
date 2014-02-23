afba63a4e7edb8905d7b0cb3e915cd1f1d4a8db3
Marc Neudert <marc.neudert@gmail.com>
2014-02-23 11:18:24 +0100

    [easy] odd numbers - c#

---
+++ easy/odd-numbers/cs.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static int Main(string[] args)
+    {
+        for (int i = 1; i < 100; i = i + 2)
+        {
+            Console.WriteLine(i);
+        }
+
+        return 0;
+    }
+}
