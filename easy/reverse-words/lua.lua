5c69200b7976252bf78222fd72a7ebe8f6e7837e
Marc Neudert <marc.neudert@gmail.com>
2014-03-20 20:50:54 +0100

    [easy] reverse words - lua

---
+++ easy/reverse-words/lua.lua
@@ -0,0 +1,17 @@
+for line in io.lines(arg[1]) do
+  local words = {}
+  local index = 0
+
+  for word in line:gmatch('(%w+)') do
+    index        = index + 1
+    words[index] = word
+  end
+
+  if index > 1 then
+    for i = index, 2, -1 do
+      io.write(words[i] .. ' ')
+    end
+  end
+
+  io.write(words[1] .. '\n')
+end
