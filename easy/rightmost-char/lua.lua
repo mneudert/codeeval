dccd654971f4ef42a8ffaf2d4f24b8330c24a0b9
Marc Neudert <marc.neudert@gmail.com>
2014-03-20 21:03:04 +0100

    [easy] rightmost char - lua

---
+++ easy/rightmost-char/lua.lua
@@ -0,0 +1,10 @@
+for line in io.lines(arg[1]) do
+  local sentence, char = line:match('(.+),(.+)')
+  local char_pos       = sentence:reverse():find(char)
+
+  if nil == char_pos then
+    io.write('-1\n')
+  else
+    io.write((sentence:len() - char_pos).. '\n')
+  end
+end
