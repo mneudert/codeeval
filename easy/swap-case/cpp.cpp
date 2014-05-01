f8be4a68edf2d72656bc69821c2d20b48352faa8
Marc Neudert <marc.neudert@gmail.com>
2014-05-01 14:41:40 +0200

    [easy] swap case - c++

---
+++ easy/swap-case/cpp.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+  FILE    *fp;
+  char    *line;
+  int      i;
+  size_t   len, read;
+
+  fp = fopen(argv[1], "r");
+
+  while (-1 != (read = getline(&line, &len, fp))) {
+    for (i = 0; line[i] != '\0'; ++i) {
+      if (65 <= line[i] && 90 >= line[i]) {
+        line[i] += 32;
+        continue;
+      }
+
+      if (97 <= line[i] && 122 >= line[i]) {
+        line[i] -= 32;
+        continue;
+      }
+    }
+
+    fputs(line, stdout);
+  }
+
+  return 0;
+}
