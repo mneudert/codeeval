6a94abe2efa86258f396a08628733fb1a144bca7
Marc Neudert <marc.neudert@gmail.com>
2014-05-16 12:40:57 +0200

    [easy] simple sorting - tcl

---
+++ easy/simple-sorting/tcl.tcl
@@ -0,0 +1,12 @@
+set file [open [lindex $argv 0] r]
+set lines [split [read $file] "\n"]
+
+foreach line $lines {
+    if {0 == [string length $line]} {
+        continue
+    }
+
+    puts [lsort -real [split $line " "]]
+}
+
+close $file
