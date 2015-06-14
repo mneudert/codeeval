f2f0a05583abc285d3a91a36602c3daa414162c1
Marc Neudert <marc.neudert@gmail.com>
2015-06-14 11:19:52 +0200

    [easy] without repetitions - js

---
+++ easy/without-repetitions/js.js
@@ -0,0 +1,9 @@
+var fs = require('fs');
+
+fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
+    if ('' === line) {
+        return;
+    }
+
+    console.log(line.replace(/(.)\1+/g, '$1'));
+});
