915289bd1a1fe644937d9e4a2f4b4942800a0f1e
Marc Neudert <marc.neudert@gmail.com>
2015-09-04 16:25:00 +0200

    [easy] reverse words - js

---
+++ easy/reverse-words/js.js
@@ -0,0 +1,9 @@
+var fs = require('fs');
+
+fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
+    if ('' === line) {
+        return;
+    }
+
+    console.log(line.trim().split(' ').reverse().join(' '));
+});
